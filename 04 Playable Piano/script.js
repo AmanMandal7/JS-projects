99ae5332037290c2d393bb55084b3cc203cdd326
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-03 10:13:13 +0545

    using keyboard to play the piano

--- 04 Playable Piano/script.js
+++ 04 Playable Piano/script.js
@@ -5,8 +5,21 @@ const audio = new Audio("tunes/a.wav"); //It creates a HTML audio element, by de
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav` // passing audio src based on key press 
     audio.play();  // playing audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); //getting clicked key element 
+    clickedKey.classList.add("active"); //adding active class to the clicked key element
+    setTimeout(() => {
+        clickedKey.classList.remove("active"); // removing active class after 150 ms from the clicked key element
+    }, 150)
+
 }
 
 pianoKeys.forEach(key => {
     key.addEventListener("click", () => playTune(key.dataset.key))
-})
+});
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
