029bca26868b55ef901ecb4c3a81cb805360a27e
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-24 09:49:40 +0545

    done with the logic building

--- 11 Random Password Generator/script.js
+++ 11 Random Password Generator/script.js
@@ -1,5 +1,6 @@
 const lenghtSlider = document.querySelector(".pass-length input");
 const options = document.querySelectorAll(".option input");
+let passwordInput = document.querySelector(".input-box input");
 const generateBtn = document.querySelector(".generate-btn");
 
 const characters = {  //object of letters, numbers and symbols
@@ -13,23 +14,39 @@ const characters = {  //object of letters, numbers and symbols
 const generatePassword = () => {
     let staticPassword = "";
     let randomPassword = "";
+    let excludeDuplicate = false;
     let passLength = lenghtSlider.value;
 
     options.forEach(option => { //looping throught each option's checkbox
         if (option.checked) {
-            staticPassword += characters[option.id];
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];
+            } else if (option.id === "spaces") {
+                staticPassword += `  ${staticPassword}  `
+            } else {
+                excludeDuplicate = true;
+            }
+
         }
     })
 
     for (let i = 0; i < passLength; i++) {
-        randomPassword += staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {
+            // if randomPassword doesn't contain the current random character or randomChar is equal
+            // to space " " then add random character to randomPassword else decrement i by -1
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
     }
 
-    console.log(randomPassword);
+    passwordInput.value = randomPassword;
 }
 
 const updateSlider = () => {
     document.querySelector(".pass-length span").innerText = lenghtSlider.value;
+    generatePassword();
 }
 updateSlider();
 
