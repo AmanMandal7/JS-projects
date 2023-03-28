bcbfa250ea28b0c2fe91d7215cc10796e2083050
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-28 10:07:55 +0545

    generating ranodm colors using refresh button

--- 13 Gradient Color Generator/script.js
+++ 13 Gradient Color Generator/script.js
@@ -2,8 +2,22 @@ const colorInputs = document.querySelectorAll(".inputs input");
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const textarea = document.querySelector("textarea");
+const refreshBtn = document.querySelector(".refresh")
 
-const generateGradient = () => {
+
+const getRandomColor = () => {
+    // Generating a random color in hexadecimal format
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${randomHex}`;
+}
+
+const generateGradient = (isRandom) => {
+    if (isRandom) {
+        // If isRandom is true, update the color inputs value with random color
+        colorInputs[0].value = getRandomColor();
+        colorInputs[1].value = getRandomColor();
+    }
+
     // Creating gradient string using select menu value with color input values
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
     gradientBox.style.background = gradient;
@@ -16,3 +30,4 @@ colorInputs.forEach(input => {
 });
 
 selectMenu.addEventListener("change", generateGradient);
+refreshBtn.addEventListener("click", () => generateGradient(true));
