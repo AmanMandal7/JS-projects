1637646be87483cc6739463eaa926078f500dd13
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-28 09:50:25 +0545

    Updating the textarea

--- 13 Gradient Color Generator/script.js
+++ 13 Gradient Color Generator/script.js
@@ -1,11 +1,13 @@
 const colorInputs = document.querySelectorAll(".inputs input");
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
+const textarea = document.querySelector("textarea");
 
 const generateGradient = () => {
     // Creating gradient string using select menu value with color input values
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
     gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient};`;
 }
 
 colorInputs.forEach(input => {
