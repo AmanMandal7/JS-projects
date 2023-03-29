6b37f60546262d6f6de3b87185dd21a711616f4a
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-29 13:22:19 +0545

    rectangle options working fine with filled option

--- 14 Paint App/script.js
+++ 14 Paint App/script.js
@@ -1,35 +1,63 @@
 const canvas = document.querySelector("canvas");
 const toolBtns = document.querySelectorAll(".tools");
+const fillColor = document.querySelector("#fill-color");
 ctx = canvas.getContext("2d");
 
+// global variables with default value
 let isDrawing = false;
+let prevMouseX, prevMouseY, snapshot;
+selectedTool = "brush";
 let brushWidth = 5;
 
 window.addEventListener("load", () => {
     // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-})
+});
+
+const drawRect = (e) => {
+    // if fill-color is checked, drawing filled rectangle else rectangle only with borders 
+    if (!fillColor.checked) {
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY); // it takes (x-cordinate, y-cordinate, width, height)
+    }
+    return ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+
+
+const startDraw = (e) => {
+    isDrawing = true;
+    prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value 
+    prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value 
+    ctx.beginPath(); // creating new path to draw
+    ctx.lineWidth = brushWidth; // setting line width
+    // copying canvas data & passing as snapshot value.. this avoids dragging the image
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
 
 const drawing = (e) => {
     if (!isDrawing) return; // if isDrawing is false return from here
-    ctx.lineTo(e.offsetX, e.offsetY); // this method creates a new line according to the mouse pointer
-    ctx.stroke(); // drawing/filling line with color
+    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
+
+    if (selectedTool === "brush") {
+        ctx.lineTo(e.offsetX, e.offsetY); // this method creates a new line according to the mouse pointer
+        ctx.stroke(); // drawing/filling line with color
+    } else if (selectedTool === "rectangle") {
+        drawRect(e);
+    }
+
 }
 
-const startDraw = () => {
-    isDrawing = true;
-    ctx.beginPath(); // creating new path to draw
-    ctx.lineWidth = brushWidth; // setting line width
-};
 
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         // removing active class from previous option and adding on current clicked option
         document.querySelector(".options .active").classList.remove("active");
         btn.classList.add("active");
-        console.log(btn.id);
-    })
+        selectedTool = btn.id;
+        console.log(selectedTool);
+    });
+
 })
 
 canvas.addEventListener("mousemove", drawing);
