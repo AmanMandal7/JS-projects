f435a908b6ef2acabe6e4494fcca9e3d600f3afc
AmanGH009 <aman.mandal.71334@gmail.com>
2023-04-04 13:45:09 +0545

    done with qr code generator

--- 15 QR Code Generator/script.js
+++ 15 QR Code Generator/script.js
@@ -1,10 +1,22 @@
 const wrapper = document.querySelector(".wrapper");
 const generateBtn = wrapper.querySelector(".form button");
 const qrInput = document.querySelector(".form input");
+const qrImg = document.querySelector(".qr-code img")
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if (!qrValue) return; // if there nothing inside input then return from here
-    console.log(qrValue);
-    wrapper.classList.add("active");
+    generateBtn.innerText = "Generating QR Code..."
+    // using qrserver api to generate new qr everytime new value is entered
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code"
+    })
+});
+
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value) {
+        wrapper.classList.remove("active");
+    }
 })
