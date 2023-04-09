ebdefa379e042f94a9b83eee46f6fed0008a41fc
AmanGH009 <aman.mandal.71334@gmail.com>
2023-04-09 13:26:34 +0545

    showing the selected qr file on the screen

--- 16 QR Code Scanner/script.js
+++ 16 QR Code Scanner/script.js
@@ -3,7 +3,7 @@ const form = wrapper.querySelector("form");
 const fileInp = form.querySelector("input");
 const infoText = form.querySelector("p");
 
-function fetchRequest(formData) {
+function fetchRequest(formData, file) {
     infoText.innerText = "Scanning QR Code...";
     // sending post request to qr server api 
     fetch("http://api.qrserver.com/v1/read-qr-code/", {
@@ -11,6 +11,7 @@ function fetchRequest(formData) {
     }).then(res => res.json()).then(result => {
         result = result[0].symbol[0].data;
         wrapper.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
         infoText.innerText = "Upload QR Code to scan";
         wrapper.classList.add("active");
     });
@@ -20,7 +21,7 @@ fileInp.addEventListener("change", e => {
     let file = e.target.files[0]; // getting user selected file
     let formData = new FormData(); // creating a new FormData object
     formData.append("file", file); // adding selected file to formData
-    fetchRequest(formData);
+    fetchRequest(formData, file);
 })
 
 form.addEventListener("click", () => fileInp.click());
