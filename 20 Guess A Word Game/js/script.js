7ec4f4eb0cf4d42605e7c71b506b55e0e83be3e5
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-21 12:43:40 +0530

    Resetting the game successfully

--- 20 Guess A Word Game/js/script.js
+++ 20 Guess A Word Game/js/script.js
@@ -1,16 +1,21 @@
 const inputs = document.querySelector(".inputs");
 const resetBtn = document.querySelector(".reset-btn");
 const hint = document.querySelector(".hint span");
+const guessesLeft = document.querySelector(".guess-left span");
+const wrongLetters = document.querySelector(".wrong-letter span");
 const typingInput = document.querySelector(".typing-input");
 
-let word;
+let word, maxGuesses, incorrects = [], corrects = [];
 
 function randomWord() {
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
     word = ranObj.word;
+    maxGuesses = 8; incorrects = []; corrects = [];
     console.log(word);
 
     hint.innerText = ranObj.hint;
+    guessesLeft.innerText = maxGuesses;
+    wrongLetters.innerText = incorrects;
 
     let html = ``;
     for (let i = 0; i < word.length; i++) {
@@ -23,7 +28,7 @@ randomWord();
 
 function initGame(e) {
     let key = e.target.value;
-    if (key.match(/^[A-Za-z]+$/)) {
+    if (key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key}`) && !corrects.includes(key)) {
         console.log(key);
         if (word.includes(key)) { // if user letter found in the word
             for (let i = 0; i < word.length; i++) {
@@ -33,11 +38,23 @@ function initGame(e) {
                 }
             }
         } else {
-            console.log("letter not found")
+            maxGuesses--;
+            incorrects.push(` ${key}`);
+
         }
+        wrongLetters.innerText = incorrects;
+        guessesLeft.innerText = maxGuesses;
     }
+
     typingInput.value = "";
 
+    if (maxGuesses < 1) { // if user couldn't found all letters
+        alert("Game Over! You don't have remaining guesses.")
+        for (let i = 0; i < word.length; i++) {
+            // show all letters in the input
+            inputs.querySelectorAll("input")[i].value = word[i];
+        }
+    }
 }
 
 resetBtn.addEventListener("click", randomWord);
