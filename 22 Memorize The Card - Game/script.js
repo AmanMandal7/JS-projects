49c4a941aab7445ebee712c5871be3a44792aaed
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-28 18:40:26 +0545

    Done with the memorize the card - game

--- 22 Memorize The Card - Game/script.js
+++ 22 Memorize The Card - Game/script.js
@@ -1,16 +1,18 @@
 const cards = document.querySelectorAll(".card");
 
+let matchedCard = 0;
 let cardOne, cardTwo;
+let disableDeck = false;
 
 function flipCard(e) {
     let clickedCard = e.target;
-    if (clickedCard !== cardOne) {
+    if (clickedCard !== cardOne && !disableDeck) {
         clickedCard.classList.add("flip");
         if (!cardOne) {
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
-
+        disableDeck = true;
         let cardOneImg = cardOne.querySelector("img").src,
             cardTwoImg = cardTwo.querySelector("img").src;
         matchCards(cardOneImg, cardTwoImg);
@@ -19,10 +21,47 @@ function flipCard(e) {
 
 function matchCards(img1, img2) {
     if (img1 === img2) {
-        return console.log("Card matched");
+        matchedCard++;
+        if (matchedCard == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
     }
-    console.log("Card not matched");
+
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
 }
+
+function shuffleCard() {
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+    disableDeck = false;
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector("img");
+        imgTag.src = `images/img-${arr[index]}.png`
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
