2dfb53c345c21cd267a60bb0d125e0e152152709
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-28 14:20:49 +0545

    matching cards

---
+++ 22 Memorize The Card - Game/script.js
@@ -0,0 +1,29 @@
+const cards = document.querySelectorAll(".card");
+
+let cardOne, cardTwo;
+
+function flipCard(e) {
+    let clickedCard = e.target;
+    if (clickedCard !== cardOne) {
+        clickedCard.classList.add("flip");
+        if (!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+
+        let cardOneImg = cardOne.querySelector("img").src,
+            cardTwoImg = cardTwo.querySelector("img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if (img1 === img2) {
+        return console.log("Card matched");
+    }
+    console.log("Card not matched");
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+})
