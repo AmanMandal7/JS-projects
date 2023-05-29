75bdee2d48277a43952f548d15049fc3868b0158
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-29 18:50:09 +0545

    Matching the characters with the typed char

---
+++ 23 Typing Speed Test - Game/js/script.js
@@ -0,0 +1,28 @@
+const typingText = document.querySelector(".typing-text p");
+const inpField = document.querySelector(".wrapper .input-field");
+
+let charIndex = 0;
+
+function randomParagraph() {
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    const characters = document.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add("correct");
+    } else {
+        characters[charIndex].classList.add("incorrect");
+    }
+    charIndex++;
+}
+
+randomParagraph();
+inpField.addEventListener("input", initTyping);
