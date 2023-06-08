0946c687fe4c406c2520aee5324c13358148f4e2
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-08 20:56:40 +0545

    Done with the Typing Speed Test - Game

--- 23 Typing Speed Test - Game/js/script.js
+++ 23 Typing Speed Test - Game/js/script.js
@@ -2,51 +2,64 @@ const typingText = document.querySelector(".typing-text p");
 const inpField = document.querySelector(".wrapper .input-field");
 const timeTag = document.querySelector(".time span b");
 const mistakeTag = document.querySelector(".mistake span");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
+const tryAgainBtn = document.querySelector("button");
 
 let timer,
     maxTime = 60,
     timeLeft = maxTime;
 isTyping = false;
 
-let charIndex = 0;
-let mistakes = 0;
+let charIndex = mistakes = 0;
 
 function randomParagraph() {
     let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
     paragraphs[randIndex].split("").forEach(span => {
         let spanTag = `<span>${span}</span>`;
         typingText.innerHTML += spanTag;
     });
+    typingText.querySelectorAll("span")[0].classList.add("active");
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
 }
 
 function initTyping() {
-    const characters = document.querySelectorAll("span");
+    const characters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
-    if (!isTyping) {
-        let timer = setInterval(initTimer, 1000);
-        isTyping = true;
-    }
-    if (typedChar == null) {
-        charIndex--;
-        if (characters[charIndex].classList.contains("incorrect")) {
-            mistakes--;
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
         }
-        characters[charIndex].classList.remove("correct", "incorrect");
-    } else {
-        if (characters[charIndex].innerText === typedChar) {
-            characters[charIndex].classList.add("correct");
+        if (typedChar == null) {
+            charIndex--;
+            if (characters[charIndex].classList.contains("incorrect")) {
+                mistakes--;
+            }
+            characters[charIndex].classList.remove("correct", "incorrect");
         } else {
-            mistakes++;
-            characters[charIndex].classList.add("incorrect");
+            if (characters[charIndex].innerText === typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
         }
-        charIndex++;
-    }
-    characters.forEach(span => span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
 
-    mistakeTag.innerText = mistakes;
+        mistakeTag.innerText = mistakes;
+        let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60);
+        // if wpm value is 0, empty or infinity then setting it's value to 0
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        wpmTag.innerText = wpm;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+    }
 }
 
 function initTimer() {
@@ -54,10 +67,25 @@ function initTimer() {
         timeLeft--;
         timeTag.innerText = timeLeft;
     } else {
+        inpField.value = "";
         clearInterval(timer);
     }
+}
+
+function resetGame() {
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer);
+    timeLeft = maxTime;
+    isTyping = false;
+    charIndex = mistakes = 0;
+    timeTag.innerText = timeLeft;
+    mistakeTag.innerText = mistakes;
+    wpmTag.innerText = 0;
+    cpmTag.innerText = 0;
 }
 
 
 randomParagraph();
 inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
