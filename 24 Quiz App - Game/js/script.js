10bb8cbceacc76e0128c915388755b3821c4d6f9
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-15 11:30:01 +0545

    Able to select the correct and incorrect answer

--- 24 Quiz App - Game/js/script.js
+++ 24 Quiz App - Game/js/script.js
@@ -3,7 +3,8 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = document.querySelector(".quiz_box")
+const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
 
 //if start quiz button clicked
 start_btn.onclick = () => {
@@ -19,20 +20,78 @@ exit_btn.onclick = () => {
 continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
-    showQuestions(4);
+    showQuestions(0);
+    queCounter(1);
 }
 
 let que_count = 0;
+let que_numb = 1;
+
+const next_btn = document.querySelector(".next_btn");
+//if Next Button clicked
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+    } else {
+        console.log("Questions Completed");
+    }
+}
+
 
 //getting questions and options from array
 function showQuestions(index) {
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
-    let que_tag = '<span>' + questions[index].question + '</span>';
+    let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
     let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div >'
         + '<div class="option"><span>' + questions[index].options[1] + '</span></div >'
         + '<div class="option"><span>' + questions[index].options[2] + '</span></div >'
         + '<div class="option"><span>' + questions[index].options[3] + '</span></div >';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+
+}
+
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer) {
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let all_options = option_list.children.length;
+    if (userAns == correctAns) {
+        answer.classList.add("correct");
+        console.log("Answer is correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    } else {
+        answer.classList.add("incorrect");
+        console.log("Answer is wrong");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        //if option is incorrect then automatically select the correct answer
+        for (let i = 0; i < all_options; i++) {
+            if (option_list.children[i].textContent == correctAns) {
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+
+    //once user selected, disable all other options
+    for (let i = 0; i < all_options; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+
+}
+
+function queCounter(index) {
+    const bottom_ques_counter = document.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>'
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
