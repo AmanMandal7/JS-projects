ef8222d38ab969b5be9b135ff62ab70ec52eda1a
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-19 15:03:18 +0545

    Working on timer

--- 25 Word Scramble - Game/js/script.js
+++ 25 Word Scramble - Game/js/script.js
@@ -1,20 +1,50 @@
 const content = document.querySelector(".content");
 const wordText = content.querySelector(".word");
-const hint = content.querySelector(".hint span");
+const inputField = document.querySelector("input");
+const timeField = document.querySelector(".time b")
+const hintText = content.querySelector(".hint span");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkBtn = document.querySelector(".check-word");
 
+let correctWord, timer;
 
+const initTimer = (maxTime) => {
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            timeField.innerText = maxTime;
+        }
+    }, 1000)
+}
 
 const initGame = () => {
+    initTimer(30);
     let randomObj = words[Math.floor(Math.random() * words.length)];
     let wordArray = randomObj.word.split("");
     for (let i = wordArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
-    console.log(randomObj);
-
     wordText.textContent = wordArray.join("");
-    hint.innerText = randomObj.hint;
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+    console.log(randomObj);
 };
 
 initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLocaleLowerCase();
+    if (!userWord) return alert("Please enter a word check");
+    if (userWord == correctWord) {
+        alert(`Conrats! ${userWord.toUpperCase()} is a correct word`);
+    } else {
+        alert(`Oops! ${userWord.toUpperCase()} is not a correct word`);
+    }
+    initGame();
+}
+
+checkBtn.addEventListener("click", checkWord)
+refreshBtn.addEventListener("click", initGame);
