d8c925beac42c4290b26c009fa7822da14bac573
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-19 14:02:04 +0545

    Displaying the word and hint

--- 25 Word Scramble - Game/js/script.js
+++ 25 Word Scramble - Game/js/script.js
@@ -1,7 +1,20 @@
+const content = document.querySelector(".content");
+const wordText = content.querySelector(".word");
+const hint = content.querySelector(".hint span");
+
+
+
 const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)];
     let wordArray = randomObj.word.split("");
-    console.log(wordArray);
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    console.log(randomObj);
+
+    wordText.textContent = wordArray.join("");
+    hint.innerText = randomObj.hint;
 };
 
 initGame();
