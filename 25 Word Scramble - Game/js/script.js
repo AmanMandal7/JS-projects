0f63f3881fced13e851b2c94cdaa39a383f5899b
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-19 13:30:31 +0545

    JS working fine

---
+++ 25 Word Scramble - Game/js/script.js
@@ -0,0 +1,7 @@
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    console.log(wordArray);
+};
+
+initGame();
