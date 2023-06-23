a91aab6ef4a9f93e7d6c5cd3179ea724f566a472
AmanGH009 <aman.mandal.71334@gmail.com>
2023-06-23 13:02:38 +0545

    Done with the Website - Login and Register

--- 26 Website - Login and Register/script.js
+++ 26 Website - Login and Register/script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrapper");
 const registerLink = document.querySelector(".register-link");
 const loginLink = document.querySelector(".login-link");
+const btnPopup = document.querySelector(".btnlogin-popup");
+const btnClose = document.querySelector(".icon-close");
 
 registerLink.addEventListener("click", () => {
     wrapper.classList.add("active");
@@ -9,4 +11,12 @@ registerLink.addEventListener("click", () => {
 loginLink.addEventListener("click", () => {
     wrapper.classList.remove("active");
 });
+
+btnPopup.addEventListener("click", () => {
+    wrapper.classList.add("active-popup");
+})
+
+btnClose.addEventListener("click", () => {
+    wrapper.classList.remove("active-popup");
+})
 
