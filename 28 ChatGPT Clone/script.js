6e4eb0dbeaae9494447d75f69248046f5a540b38
AmanGH009 <aman.mandal.71334@gmail.com>
2023-07-06 21:14:11 +0545

    Working on OpenAi API to get the response

---
+++ 28 ChatGPT Clone/script.js
@@ -0,0 +1,45 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+
+let userText = null;
+
+const createElement = (html, className) => {
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = html;
+    return chatDiv;
+}
+
+const showTypingAnimation = () => {
+    const html = `<div class="chat-content">
+          <div class="chat-details">
+            <img src="images/chatbot.jpg" alt="user-img" />
+            <div class="typing-animation">
+              <div class="typing-dot" style="--delay: 0.2s"></div>
+              <div class="typing-dot" style="--delay: 0.3s"></div>
+              <div class="typing-dot" style="--delay: 0.4s"></div>
+            </div>
+          </div>
+          <span class="material-symbols-rounded"> content_copy </span>
+        </div>`
+
+    const incomingChatDiv = createElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim(); // get chatInput value and remove extra spaces
+    const html = `<div class="chat-content">
+          <div class="chat-details">
+            <img src="images/user.jpg" alt="user-img" />
+            <p>${userText}</p>
+          </div>
+        </div>`;
+    const outgoingChatDiv = createElement(html, "outgoing");
+    chatContainer.appendChild(outgoingChatDiv);
+    setTimeout(showTypingAnimation, 500);
+}
+
+sendButton.addEventListener("click", handleOutgoingChat);
+
