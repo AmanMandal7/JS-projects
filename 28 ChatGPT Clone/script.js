853ad7701e94141ba792e5c02a595b1208faca89
AmanGH009 <aman.mandal.71334@gmail.com>
2023-07-06 21:59:00 +0545

    Done with generating the actual response

--- 28 ChatGPT Clone/script.js
+++ 28 ChatGPT Clone/script.js
@@ -3,12 +3,44 @@ const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 
 let userText = null;
+const API_KEY = "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";
 
 const createElement = (html, className) => {
     const chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
     chatDiv.innerHTML = html;
     return chatDiv;
+}
+
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL = "https://api.openai.com/v1/completions";
+    const pElement = document.createElement("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: userText,
+            max_tokens: 2048,
+            temperature: 0.2,
+            n: 1,
+            stop: null
+        })
+    }
+
+    try {
+        const response = await (await fetch(API_URL, requestOptions)).json();
+        pElement.textContent = response.choices[0].text.trim();
+    } catch (error) {
+        console.log(error);
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
 }
 
 const showTypingAnimation = () => {
@@ -26,17 +58,22 @@ const showTypingAnimation = () => {
 
     const incomingChatDiv = createElement(html, "incoming");
     chatContainer.appendChild(incomingChatDiv);
+    getChatResponse(incomingChatDiv);
 }
 
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim(); // get chatInput value and remove extra spaces
+    if (!userText) return; // If chatInput is empty return from here
+
     const html = `<div class="chat-content">
           <div class="chat-details">
             <img src="images/user.jpg" alt="user-img" />
-            <p>${userText}</p>
+            <p></p>
           </div>
         </div>`;
+
     const outgoingChatDiv = createElement(html, "outgoing");
+    outgoingChatDiv.querySelector("p").textContent = userText;
     chatContainer.appendChild(outgoingChatDiv);
     setTimeout(showTypingAnimation, 500);
 }
