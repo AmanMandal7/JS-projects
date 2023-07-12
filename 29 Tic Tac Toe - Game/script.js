2fbf82860ac29a19f5ebdb96ea5dface797bc885
AmanGH009 <aman.mandal.71334@gmail.com>
2023-07-12 21:40:27 +0545

    Starting with the player section

--- 29 Tic Tac Toe - Game/script.js
+++ 29 Tic Tac Toe - Game/script.js
@@ -1,4 +1,31 @@
 const selectBox = document.querySelector(".select-box");
 const selectXBtn = selectBox.querySelector(".playerX");
 const selectOBtn = selectBox.querySelector(".playerO");
+const playBoard = document.querySelector(".play-board");
+const allBox = document.querySelectorAll("section span");
+const players = document.querySelector(".players");
 
+window.onload = () => {
+
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onClick", "clickedBox(this)");
+    }
+
+    selectXBtn.onclick = () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+    }
+    selectOBtn.onclick = () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+    }
+}
+
+let playerXIcon = "fas fa-times";
+let playerOIcon = "fas fa-circle";
+
+function clickedBox(element) {
+    if () {
+
+    }
+}
