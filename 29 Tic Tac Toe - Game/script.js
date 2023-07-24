80ba848ac7d0259d4b6e61121548ec66bd02f6bd
AmanGH009 <aman.mandal.71334@gmail.com>
2023-07-24 19:01:41 +0545

    Done with players selection

--- 29 Tic Tac Toe - Game/script.js
+++ 29 Tic Tac Toe - Game/script.js
@@ -5,10 +5,12 @@ const playBoard = document.querySelector(".play-board");
 const allBox = document.querySelectorAll("section span");
 const players = document.querySelector(".players");
 
+
+
 window.onload = () => {
 
     for (let i = 0; i < allBox.length; i++) {
-        allBox[i].setAttribute("onClick", "clickedBox(this)");
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 
     selectXBtn.onclick = () => {
@@ -18,15 +20,20 @@ window.onload = () => {
     selectOBtn.onclick = () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
-        players.setAttribute("class", "players active");
+        players.setAttribute("class", "players active player");
     }
 }
 
-let playerXIcon = "fas fa-times";
-let playerOIcon = "fas fa-circle";
+let playerXIcon = "fa fa-times";
+let playerOIcon = "fa-regular fa-circle";
 
-// function clickedBox(element) {
-//     if () {
-
-//     }
-// }
+function clickedBox(element) {
+    if (players.classList.contains("player")) {
+        element.innerHTML = `<i class="${playerOIcon}"></i>`
+        players.classList.add("active");
+    } else {
+        element.innerHTML = `<i class="${playerXIcon}"></i>`
+        players.classList.add("active");
+    }
+    element.style.pointerEvents = "none";
+}
