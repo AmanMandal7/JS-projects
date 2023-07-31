12f68568f9745b90bd278eb38bfc8d44176ef608
AmanGH009 <aman.mandal.71334@gmail.com>
2023-07-31 18:36:21 +0545

    Done with the bot selection feature

--- 29 Tic Tac Toe - Game/script.js
+++ 29 Tic Tac Toe - Game/script.js
@@ -42,12 +42,22 @@ function clickedBox(element) {
 
 // bot click function
 function bot() {
-    let array = []; //storing the indexed of not selected boxes
+    let array = []; //storing the indexes of not selected boxes
     for (let i = 0; i < allBox.length; i++) {
         if (allBox[i].childElementCount == 0) {
             array.push(i);
-            console.log(i + " " + "has no children");
+            // console.log(i + " " + "has no children");
         }
     }
-    console.log(array);
+    let randomBox = array[Math.floor(Math.random() * array.length)];
+    // console.log(randomBox);
+    if (array.length > 0) {
+        if (players.classList.contains("player")) {
+            allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`
+            players.classList.add("active");
+        } else {
+            allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`
+            players.classList.add("active");
+        }
+    }
 }
