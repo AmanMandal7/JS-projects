76bc01cbe2ad38eb7d738635215b87750bd9b172
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-10 20:52:16 +0545

    Done with some scripting of the file

---
+++ 30 Hangman - Game/scripts/script.js
@@ -0,0 +1,34 @@
+const keyboardDiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".word-display");
+
+let currectWord;
+
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    currectWord = word;
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+const initGame = (button, clickedLetter) => {
+    if (currectWord.includes(clickedLetter)) {
+        [...currectWord].forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+    } else {
+        console.log(clickedLetter, "does not exist on the word");
+    }
+}
+
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
