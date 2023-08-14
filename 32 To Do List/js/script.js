d3e7a48e3c62d7a1ec132da5ef5068ae6ee38df5
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-14 13:21:59 +0545

    Done with overall functions

---
+++ 32 To Do List/js/script.js
@@ -0,0 +1,43 @@
+const inputField = document.querySelector(".input-field textarea");
+const todoLists = document.querySelector(".todoLists");
+const pendingNum = document.querySelector(".pending-number");
+const clearBtn = document.querySelector(".clear-button");
+
+
+function allTasks() {
+    const tasks = document.querySelectorAll(".pending");
+    pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+}
+
+inputField.addEventListener("keyup", (e) => {
+    let inputValue = inputField.value.trim(); //trim function trims the space in front and back of the content
+
+    if (e.key === "Enter" && inputValue.length > 0) {
+        let liTag = `<li class="list pending" onclick="handleStatus(this)"">
+          <input type="checkbox"/>
+          <span class="task">${inputValue}</span>
+          <i class="uil uil-trash" onclick="deleteTask(this)"></i>
+        </li>`
+
+        todoLists.insertAdjacentHTML("beforeend", liTag);
+        inputField.value = "";
+        allTasks();
+    }
+});
+
+function handleStatus(e) {
+    const checkbox = e.querySelector("input");
+    checkbox.checked = checkbox.checked ? false : true;
+    e.classList.toggle("pending");
+    allTasks();
+}
+
+function deleteTask(e) {
+    e.parentElement.remove();
+    allTasks();
+}
+
+clearBtn.addEventListener("click", () => {
+    todoLists.innerHTML = "";
+    allTasks();
+})
