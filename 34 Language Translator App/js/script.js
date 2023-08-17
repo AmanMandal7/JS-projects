5d9d2f8ada0ef0610a11f570abe96cc3f38dd75d
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-17 16:18:47 +0545

    Done with translation of the text

--- 34 Language Translator App/js/script.js
+++ 34 Language Translator App/js/script.js
@@ -1,4 +1,7 @@
 const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
     for (const coutry_code in countries) {
@@ -11,4 +14,15 @@ selectTag.forEach((tag, id) => {
         let option = `<option value="${coutry_code}" ${selected}>${countries[coutry_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
+})
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+        translateFrom = selectTag[0].value,
+        translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+    })
 })
