c47fe50fe697839a68500747fc10c9c6761d2514
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-18 10:34:05 +0545

    Done with copy button

--- 34 Language Translator App/js/script.js
+++ 34 Language Translator App/js/script.js
@@ -1,7 +1,9 @@
 const selectTag = document.querySelectorAll("select");
 const fromText = document.querySelector(".from-text");
 const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
 const translateBtn = document.querySelector("button");
+const icons = document.querySelectorAll(".row i");
 
 selectTag.forEach((tag, id) => {
     for (const coutry_code in countries) {
@@ -14,7 +16,17 @@ selectTag.forEach((tag, id) => {
         let option = `<option value="${coutry_code}" ${selected}>${countries[coutry_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
-})
+});
+
+exchangeIcon.addEventListener("click", () => {
+    let temText = fromText.value;
+    fromText.value = toText.value;
+    toText.value = temText;
+
+    let tempLanguage = selectTag[0].value;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLanguage;
+});
 
 translateBtn.addEventListener("click", () => {
     let text = fromText.value,
@@ -22,7 +34,21 @@ translateBtn.addEventListener("click", () => {
         translateTo = selectTag[1].value;
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`
     fetch(apiUrl).then(res => res.json()).then(data => {
-        console.log(data);
         toText.value = data.responseData.translatedText;
+    })
+});
+
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({ target }) => {
+        if (target.classList.contains("fa-copy")) {
+            if (target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else if (target.id == "to") {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            console.log("Speech icon clicked")
+        }
     })
 })
