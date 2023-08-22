b1e55239c91cac269e01169f3e15235eca50b977
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-22 11:28:12 +0545

    Starting with the animations

--- 35 Responsive Camping Website/js/main.js
+++ 35 Responsive Camping Website/js/main.js
@@ -18,3 +18,12 @@ if (navClose) {
 navLink.forEach(n => n.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
 }));
+
+// On scroll add background header 
+const bgHeader = () => {
+    const header = document.querySelector(".header")
+    // When scrolling and the viewport height is more than 50, then add bg-header 
+    this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header")
+}
+
+window.addEventListener("scroll", bgHeader);
