cbd765dd74c1b7ecf874348b6f1d9a9188a48772
AmanGH009 <aman.mandal.71334@gmail.com>
2023-08-24 11:31:00 +0545

    API working fine

--- 36 Weather App/script.js
+++ 36 Weather App/script.js
@@ -1,14 +1,18 @@
 const cityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-btn");
 
-const API_KEY = "";
+const API_KEY = "06c380a418fe587a137dfeba46351232";
 
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); //remove the extra spaces
     if (!cityName) return;
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=06c380a418fe587a137dfeba46351232`
 
-    console.log(cityName);
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!")
+    })
 }
 
 searchBtn.addEventListener("click", getCityCoordinates);
