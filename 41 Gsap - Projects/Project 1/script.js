4594fc2c44b5d8faae093e3f6d9133a1e99ed71f
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-10 11:00:26 +0545

    Done with all the animations

--- 41 Gsap - Projects/Project 1/script.js
+++ 41 Gsap - Projects/Project 1/script.js
@@ -4,11 +4,47 @@ tl.from(".nav img, .nav .nav3", {
     y: -100,
     duration: 1,
     delay: 1,
+    stagger: .5,
     opacity: 0
 });
 
 tl.from(".nav .nav2", {
     y: 50,
     duration: 1,
-    opacity: 0
+    opacity: 0,
+    delay: -.5
 })
+
+tl.from(".headings h1", {
+    y: 200,
+    opacity: 0,
+    delay: -0.5,
+    duration: 1,
+    stagger: .5
+})
+
+tl.from(".headings h1 span", {
+    x: -400,
+    opacity: 0,
+    duration: .4
+})
+
+tl.from(".images img", {
+    scale: 0,
+    duration: 1,
+    delay: -.5
+})
+
+tl.from("p", {
+    y: 200,
+    opacity: 0,
+    duration: 1
+})
+
+tl.to("p", {
+    y: 8,
+    repeat: -1,
+    duration: 1,
+    yoyo: true
+})
+
