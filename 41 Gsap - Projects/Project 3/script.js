be26becaa96b32556453088b8756044b9890dc8f
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-13 11:58:33 +0545

    Styled and animated the first page

--- 41 Gsap - Projects/Project 3/script.js
+++ 41 Gsap - Projects/Project 3/script.js
@@ -1,5 +1,15 @@
-gsap.to(".page1 h1", {
-    transform: "translateX(-142%)",
-    duration: 2,
-    delay: 1
-})
+var tl = gsap.timeline();
+
+tl.to(".page1 h1", {
+    transform: "translateX(-1900px)",
+    fontWeight: 200,
+    scrollTrigger: {
+        trigger: ".page1",
+        scroller: "body",
+        // markers: true,
+        start: "top 0%",
+        end: "top -200%",
+        scrub: 2,
+        pin: true //when we are using pin, we must trigger the parent page
+    },
+});
