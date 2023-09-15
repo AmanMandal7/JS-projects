e40adae7438a73d9e0c1792545e3b11b523269c4
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-15 10:33:47 +0545

    Done with Project 3

--- 41 Gsap - Projects/Project 3/script.js
+++ 41 Gsap - Projects/Project 3/script.js
@@ -1,23 +1,33 @@
 var tl = gsap.timeline();
 
+const load = false;
 const loadingTimer = () => {
     var a = 0;
     setInterval(() => {
         if (a < 100) {
-            a = a + Math.floor(Math.random() * 15);
+            a = a + Math.floor(Math.random() * 20);
             document.querySelector(".loader h1").innerHTML = a + "%"
         } else {
             document.querySelector(".loader h1").innerText = `100%`
+
         }
     }, 150);
-}
-loadingTimer();
+};
 
-// tl.to(".loader", {
-//     top: "-100vh",
-//     delay: 1,
-//     duration: 1
-// })
+
+tl.to(".loader h1", {
+    delay: .5,
+    duration: 1.5,
+    onStart: loadingTimer()
+})
+
+tl.to(".loader", {
+    top: "-100vh",
+    delay: .5,
+    duration: 1,
+})
+
+
 
 tl.to(".page1 h1", {
     transform: "translateX(-1900px)",
