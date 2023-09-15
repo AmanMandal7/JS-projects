a79202ad6c47e632f3de10ddab657a24098cf39a
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-15 09:56:08 +0545

    Done with scripting the loading part

--- 41 Gsap - Projects/Project 3/script.js
+++ 41 Gsap - Projects/Project 3/script.js
@@ -1,7 +1,20 @@
 var tl = gsap.timeline();
 
+const loadingTimer = () => {
+    var a = 0;
+    setInterval(() => {
+        if (a < 100) {
+            a = a + Math.floor(Math.random() * 15);
+            document.querySelector(".loader h1").innerHTML = a + "%"
+        } else {
+            document.querySelector(".loader h1").innerText = `100%`
+        }
+    }, 150);
+}
+loadingTimer();
+
 // tl.to(".loader", {
-//     top: "-100%",
+//     top: "-100vh",
 //     delay: 1,
 //     duration: 1
 // })
