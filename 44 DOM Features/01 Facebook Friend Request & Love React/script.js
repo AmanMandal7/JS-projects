b3bad80e5a5f2fecd60c417926631635059a8bd1
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-19 12:06:46 +0545

    Done with the Facebook Friend Request & Love React

--- 44 DOM Features/01 Facebook Friend Request & Love React/script.js
+++ 44 DOM Features/01 Facebook Friend Request & Love React/script.js
@@ -1,31 +1,63 @@
 const addFriend = document.querySelector(".add");
 const remove = document.querySelector(".remove");
+const img = document.querySelector(".card img");
+const love1 = document.querySelector(".card>i");
 const love2 = document.querySelector(".name span");
 const love2Icon = document.querySelector(".name span i");
 
+var friends = false;
 addFriend.addEventListener("click", () => {
     addFriend.innerHTML = `<i class="ri-check-line"></i> Friends`;
     addFriend.style.pointerEvents = "none";
     remove.style.pointerEvents = "initial";
+    friends = true;
 })
 
 remove.addEventListener("click", () => {
     addFriend.innerHTML = `Add Friend`;
     addFriend.style.pointerEvents = "initial";
     remove.style.pointerEvents = "none";
+    friends = false;
 });
 
-// For Love Reacts 
-var love = false;
-love2.addEventListener("click", () => {
-    if (!love) {
+// For Love Reacts
+
+img.addEventListener("dblclick", () => {
+    if (friends) {
+        love1.style.opacity = '0.8';
+        love1.style.transform = 'scale(1.3)'
+
         love2.innerHTML = `<i class="ri-heart-3-fill"></i>`
         love2.style.color = 'red'
         love = true;
+        setTimeout(() => {
+            love1.style.transform = 'scale(1)'
+        }, 300);
+        setTimeout(() => {
+            love1.style.opacity = '0';
+        }, 1500);
+    }
+})
+
+
+var love = false;
+love2.addEventListener("click", () => {
+    if (friends) {
+        setTimeout(() => {
+            love2.style.transform = `scale(1)`
+        }, 300);
+        if (!love) {
+            love2.innerHTML = `<i class="ri-heart-3-fill"></i>`
+            love2.style.color = 'red'
+            love2.style.transform = `scale(1.2)`
+            love = true;
+        } else {
+            love2.innerHTML = `<i class="ri-heart-3-line"></i>`
+            love2.style.color = 'black'
+            love = false;
+        }
     } else {
         love2.innerHTML = `<i class="ri-heart-3-line"></i>`
         love2.style.color = 'black'
-        love = false;
     }
-
 })
