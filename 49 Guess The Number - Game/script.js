f0ac210ce9dfc0cc0a572be689edb83a488bd7d3
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-26 11:59:19 +0545

    Done with the html and css

---
+++ 49 Guess The Number - Game/script.js
@@ -0,0 +1,6 @@
+// const message = document.querySelector(".message");
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//     message.style.display = "none"
+// })
